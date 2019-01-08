dcaf1b6b609b6ebf0652a993a9e9263c23f683f3
Behuamuh <sven1980@list.ru>
2019-01-08 14:37:26 +0300

    done array cardio day 2

---
+++ 07 - Array Cardio Day 2/script.js
@@ -0,0 +1,51 @@
+// ## Array Cardio Day 2
+
+const people = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 }
+];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+
+const someOneOlder = people.some(
+  ({ year }) => new Date().getFullYear() - year >= 19
+);
+
+console.log(someOneOlder);
+
+// Array.prototype.every() // is everyone 19 or older?
+
+const everyOneOlder = people.every(
+  ({ year }) => new Date().getFullYear() - year >= 19
+);
+
+console.log(everyOneOlder);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+const commentWithId = comments.find(({ id }) => id === 823423);
+
+console.log(commentWithId);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
+const index = comments.findIndex(({ id }) => id === 823423);
+
+comments.splice(index, 1);
+
+console.table(comments);
